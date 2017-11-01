ca7ab706e166d152677f5ab39006e8e539c13857
Constantinos Yenis <cyenis@outlook.com>
2017-11-01 09:28:44 +0100

    third

--- app.js
+++ app.js
@@ -2,25 +2,106 @@
 
 // function App(container), buildSplash(), buildModes(), buildRoulette(), buildResult()
 
-function App() {
+function App(main) {
+  var self = this;
 
+  self.modes = [{
+    label: 'Travel',
+    className: 'travel',
+    imgUrl: 'http://'
+  }, {
+    label: 'Food',
+    className: 'food',
+    imgUrl: 'http://'
+  }];
+
+  self.main = main;
 }
 
-function buildSplash() {
+//function buildSplash() {}
 
-}
 
-function buildModes() {
+App.prototype.buildSplash = function() {
+  var self = this;
 
-}
+  var divAround = document.createElement('div');
+  divAround.setAttribute('id', 'div-around');
+  self.main.appendChild(divAround);
 
-function buildRoulette()() {
+  // Name of the Game:
+  var text = document.createElement('h1');
+  text.innerHTML = 'Roda!'.toUpperCase();
+  text.setAttribute('class', 'padding-start-screen');
+  divAround.appendChild(text);
 
-}
 
-function buildResult() {
+  App.prototype.description = function() {
+    console.log('Life is the sum of random facts!');
+  };
 
-}
+  // Display the Start-Button:
+  var startButton = document.createElement('button');
+  startButton.innerHTML = "Let's roll!".toUpperCase();
+  startButton.setAttribute('class', 'button');
+  startButton.setAttribute('id', 'attention-btn');
+  divAround.appendChild(startButton);
+
+  // Display the Rules-Button:
+  // var rulesButton = document.createElement('button');
+  // rulesButton.innerHTML = "Rules".toUpperCase();
+  // rulesButton.setAttribute('class', 'button');
+  // rulesButton.setAttribute('id', 'ghost-btn');
+  // divAround.appendChild(rulesButton);
+  //Add Click-Events:
+  startButton.addEventListener('click', function() {
+    divAround.remove();
+    self.buildModes();
+  });
+  //
+  // resetButton.addEventListener('click', function() {
+  //   self.rules();
+  // });
+
+};
+
+
+
+
+App.prototype.buildModes = function() {
+  var self = this;
+
+  var divAround = document.createElement('div');
+  divAround.setAttribute('id', 'div-around');
+  self.main.appendChild(divAround);
+
+  for (var ix = 0; ix < self.modes.length; ix++) {
+    var mode = document.createElement('div');
+    mode.classList.add(self.modes[ix].className);
+
+    var text = document.createElement('h2');
+    text.innerText = self.modes[ix].label;
+
+    //mode.appendChild(img);
+    mode.appendChild(text);
+
+    mode.setAttribute('data-index', ix);
+    mode.addEventListener('click', function(event) {
+      divAround.remove();
+      self.buildRoulette(event.currentTarget.getAttribute('data-index'));
+    });
+
+    divAround.appendChild(mode);
+  }
+};
+
+App.prototype.buildRoulette = function(modeIndex) {
+  console.log(modeIndex);
+
+};
+
+App.prototype.buildResult = function() {
+
+};
 
 
 
--- init.js
+++ init.js
@@ -1,4 +1,20 @@
 'use strict';
 // window on dom content loaded > container = get element by id, app = new App(container) > app.buildSplash()
 
+
+$(document).ready(function() {
+  var main = document.getElementById("main");
+  var app = new App(main);
+  app.buildSplash();
+  console.log('ready');
+});
+
+
+/*
+document.getElementById("container");
+
+
 var app = new App();
+
+var test = 2;
+*/
